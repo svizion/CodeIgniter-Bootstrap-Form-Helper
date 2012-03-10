9b0bae2429adea8e213eeb821e5cb8ad2c36b0eb
Shawn Crigger <support@s-vizion.com>
2012-03-10 14:58:35 -0500

    Fixing double form_search

--- application/helpers/MY_form_helper.php
+++ application/helpers/MY_form_helper.php
@@ -147,11 +147,8 @@ if ( ! function_exists('form_search'))
 
 if ( ! function_exists('form_date'))
 {
-		function form_search($data='', $value='', $label='', $extra='', $tooltip = '' )
+		function form_date($data='', $value='', $label='', $extra='', $tooltip = '' )
 		{
-				return _form_common('date', $data, $value, $label, $extra);
+				return _form_common('date', $data, $value, $label, $extra, $tooltip = '' )
 		}
 }
-
-
-
